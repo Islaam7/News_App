2bf3a7f311936479804c22c16eb42e0622ae79b5
Eslam Fares Mohamed Emam <152084165+bgbos7077@users.noreply.github.com>
2024-10-30 19:33:10 +0300

    Repalced ListView with sliverList

--- lib/component/news_card.dart
+++ lib/component/news_card.dart
@@ -1,40 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/models/news_data.dart';
+
 class NewsCard extends StatelessWidget {
   final NewsData newsData = NewsData();
-
 
   NewsCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    return Container(
-          height:500,
-          child: ListView.builder(
-            itemCount: newsData.news.length,
-            itemBuilder: (context, index) {
-              return Column(
-
-                children: [
-                  Container(
-                    width: double.infinity,
-                    height: 400,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        image: DecorationImage(image: newsData.news[index].imageUrl, fit: BoxFit.cover)
-                    ),
-                  ),
-                  ListTile(
-                    contentPadding: EdgeInsets.zero,
-                    title: Text('${newsData.news[index].title}'),
-                    subtitle: Text('${newsData.news[index].subTitle}'),
-                  ),
-                  SizedBox(height: 25)
-                ],
-              );
-            }
+    return SliverList(
+        delegate: SliverChildBuilderDelegate(childCount: newsData.news.length,
+            (context, index) {
+      return Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 400,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                image: DecorationImage(
+                    image: newsData.news[index].imageUrl, fit: BoxFit.cover)),
           ),
-        );
+          ListTile(
+            contentPadding: EdgeInsets.zero,
+            title: Text(
+              '${newsData.news[index].title}',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            subtitle: Text('${newsData.news[index].subTitle}'),
+          ),
+          SizedBox(height: 25)
+        ],
+      );
+    }));
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/component/category_list.dart';
 import 'package:news_app/component/news_card.dart';
+
 class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,11 +35,15 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            CategoryList(),
-            SizedBox(height: 50),
+        child: CustomScrollView(
+          physics: BouncingScrollPhysics(),
+          slivers: [
+            SliverToBoxAdapter(
+              child: CategoryList()
+            ),
+            SliverToBoxAdapter(child: SizedBox(height: 50)),
             NewsCard(),
+
           ],
         ),
       ),
